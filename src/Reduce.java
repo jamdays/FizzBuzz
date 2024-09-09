acf3ecc570059c39b069f520bd2b2dde6b8b0908
jamdays <venuedollar@protonmail.com>
2024-09-09 18:22:12 -0400

    Reduce created and finished

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int c = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else{
+                n -= 1;
+            }
+            c++;
+        }
+        System.out.println(c);
+    }
 }
